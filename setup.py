fc08ffd480922468003e4fc949369c6bff781f06
rnarule <rnarule@gmail.com>
2024-04-23 12:32:09 +0530

    data added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Twitter_Hate_Speech_Classification",
+    version="0.0.1",
+    author="rnarule",
+    author_email="rnarule@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
